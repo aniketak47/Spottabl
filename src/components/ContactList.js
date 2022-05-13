306760a25ed3ccc5bfcf24802bc8dcb4c6963ca2
aniketak47 <aniketkr20012019@gmail.com>
2022-05-13 10:06:34 +0530

    small update in search

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import {Link} from "react-router-dom";
 import ContactCard from "./ContactCard";
+import './App.css';
 
 const ContactList = (props) => {
   console.log(props);
@@ -8,27 +9,27 @@ const ContactList = (props) => {
   const deleteConactHandler = (id) => {
     props.getContactId(id);
   };
-
+  
   
   const renderContactList = props.contacts.map((contact) => {
     return (
       <ContactCard
-        contact={contact}
-        clickHander={deleteConactHandler}
-        key={contact.id}
+      contact={contact}
+      clickHander={deleteConactHandler}
+      key={contact.id}
       />
-    );
-  });
-
-  const getSearchTerm = () => {
-    props.searchKeyword(inputEl.current.value);
-  };
-  return (
-    <div className="main">
+      );
+    });
+    
+    const getSearchTerm = () => {
+      props.searchKeyword(inputEl.current.value);
+    };
+    return (
+      <div className="main">
       <h3 style={{marginLeft:"3.5rem"}}>Customer Success Managers <Link to="/add"><button className="ui button blue right" style={{marginLeft:"48rem"}}>Add CSM</button></Link></h3>
       <div className="ui search">
         <div className="ui icon input">
-          <input ref={inputEl} type="text" placeholder="Search by Name or email" className="prompt" value={ props.term} onChange={ getSearchTerm }/>
+          <input ref={inputEl} type="text" placeholder="Search by Name or email" className="prompt searchBar" value={ props.term} onChange={ getSearchTerm } style={{width:"1000px",marginLeft:"3.8rem"}}/>
           <i className="search icon"></i>
         </div>
       </div>
